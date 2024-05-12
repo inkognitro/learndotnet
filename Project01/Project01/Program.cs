100fda7cda23d76142a1aceb750e690c8932b003
Maece Fischer <contact@maecefischer.ch>
2024-05-12 11:36:22 +0200

    init project 1

---
+++ Project01/Project01/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

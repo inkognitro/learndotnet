126b70739778d013d7f4445de44e0fa858426c26
Maece Fischer <contact@maecefischer.ch>
2024-05-18 12:54:13 +0200

    additional experimental project

---
+++ Project02/Project02/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] emails = new string[5]{
+ "foo1@bar.com",
+ "foo2@bar.com",
+ "foo3@bar.com",
+ "foo4@bar.com",
+ "foo5@bar.com",
+};
+
+foreach(string email in emails)
+{
+    Console.WriteLine(email);
+}
